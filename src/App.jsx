e2f0dda6dcf2d4938897c7aab0557692b0a9074a
SillyCatto <raiyanmuhtasim@gmail.com>
2024-12-28 20:05:36 +0600

    added redux store and userSlice

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,22 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Body from "./Body";
 import Login from "./Login";
 import Profile from "./Profile.jsx";
+import { Provider } from "react-redux";
+import { appStore } from "./utils/store.js";
 
 export default function App() {
   return (
     <>
-      <BrowserRouter basename="/">
-        <Routes>
-          <Route path="/" element={<Body />}>
-            <Route path="/profile" element={<Profile />} />
-          </Route>
-          <Route path="/login" element={<Login />} />
-        </Routes>
-      </BrowserRouter>
+      <Provider store={appStore}>
+        <BrowserRouter basename="/">
+          <Routes>
+            <Route path="/" element={<Body />}>
+              <Route path="/profile" element={<Profile />} />
+            </Route>
+            <Route path="/login" element={<Login />} />
+          </Routes>
+        </BrowserRouter>
+      </Provider>
     </>
   );
 }
